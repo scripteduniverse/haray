df907fa96920cde9031f5a9cb095a821b41b0edd
elnar2 <elnar@tor.az>
2020-12-09 16:56:54 +0400

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.scss";
 import Layout from "./components/Layout/Layout";
 import FaqMain from "./components/Faq/Faq";
 import Nav from "./components/Nav/Nav";
-import Test from "./components/test/test";
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
-    // <Test />
   );
 }
 
--- src/components/test/test.js
+++ src/components/test/test.js
@@ -1,30 +1,8 @@
-import React, { useRef, useEffect } from "react";
-import { TweenMax, Power3 } from "gsap";
+import React from "react";
+
 import "./teststyles.scss";
-import { Rotate90DegreesCcwRounded } from "@material-ui/icons";
-import transitions from "@material-ui/core/styles/transitions";
 
 function Test() {
-  // let logoItem = useRef(null);
-
-  // useEffect(() => {
-  //   TweenMax.to(logoItem, 0.8, {
-  //     y: -20,
-  //     ease: "elastic",
-  //     rotation: 360,
-  //     totalTime: 10,
-  //     repeatDelay: 2,
-  //     repeat: 100,
-  //   });
-  //   TweenMax.to(".x91", 0.8, {
-  //     rotation: 27,
-  //     x: 200,
-  //     y: 100,
-  //     duration: 1,
-  //     repeat: 100,
-  //   });
-  // }, []);
-
   return (
     <div className="x91">
       <div className="x92"></div>​<div class="x93"></div>​
